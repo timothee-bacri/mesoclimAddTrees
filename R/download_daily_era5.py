468ac2e5fee62fee477203240b5d96e029bcb068
Mosedale <J.Mosedale@exeter.ac.uk>
2024-10-07 16:40:02 +0100

    Testing updates

---
+++ R/download_daily_era5.py
@@ -0,0 +1,26 @@
+# https://github.com/ecmwf/cdsapi
+# https://cds.climate.copernicus.eu/cdsapp#!/software/app-c3s-daily-era5-statistics?tab=appcode
+import cdsapi
+c = cdsapi.Client()
+MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]  
+
+for month in MONTHS:
+	result = c.service("tool.toolbox.orchestrator.workflow",
+				params={"realm": "user-apps",
+				 "project": "app-c3s-daily-era5-statistics",
+				 "version": "master",
+				 "kwargs": {"dataset": "reanalysis-era5-single-levels", 
+							"product_type": "reanalysis",
+							"variable": "2m_temperature",
+							"statistic": "daily_maximum",
+							"year": "2020",
+							"month": month,
+							"time_zone": "UTC+00:00",
+							"frequency": "1-hourly",
+							"grid": "0.25/0.25",
+							"area": {"lat": [49.5, 61], "lon": [-9, 3]}
+							},
+				 "workflow_name": "application"
+				 })
+	c.download(result)
+	#c.download(result,targets='era5daily.nc')
