962c925ce990bff1e97fb2f1674941acf4914af3
Mosedale <J.Mosedale@exeter.ac.uk>
2024-10-15 11:40:36 +0100

    Update Namespace

--- inst/extdata/load_era5.R
+++ inst/extdata/load_era5.R
@@ -1,9 +1,10 @@
+library(ecmwfr)
+
 dir_out<-"D:/era5"
 cds_key<-"664bf7b4-9bc2-4ce5-b479-6b731c7a4e31"
-library(ecmwfr)
 wf_set_key(key=cds_key,user='J.Mosedale@exeter.ac.uk')
 
-for(yr in 2019:2023){
+for(yr in c(2010,2012)){
   print(yr)
   download_era5(dir_out, file_out=paste0("era5_surface_ukeire_",yr,".nc"),
                           yr, area = c(61, -11.25, 48.75, 2.5),
